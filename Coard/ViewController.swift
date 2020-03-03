bc6e54b4d73d0e1277a1ecb525114555b0c6c17f
Raymond Tellier <ray@frap.io>
2020-03-02 23:07:52 -0500

    Initial Commit

---
+++ Coard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coard
+//
+//  Created by Raymond Tellier on 2020-03-02.
+//  Copyright © 2020 Frap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
