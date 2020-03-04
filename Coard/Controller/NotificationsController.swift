7d88d1e1f34203f41723312ec569856abbfe3fdd
Raymond Tellier <ray@frap.io>
2020-03-03 20:55:52 -0500

    Setup navigation bars

--- Coard/Controller/NotificationsController.swift
+++ Coard/Controller/NotificationsController.swift
@@ -15,9 +15,12 @@ class NotifcationsController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemPink
+        configureUI()
     }
     
     // MARK: - Helpers
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
+    }
 }
