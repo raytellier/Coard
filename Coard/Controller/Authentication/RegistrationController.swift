622c7bc7bec1e415d1e52513fb91a9181c89fee1
Raymond Tellier <ray@frap.io>
2020-03-08 17:45:02 -0400

    setup login ui

---
+++ Coard/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,27 @@
+//
+//  RegistrationController.swift
+//  Coard
+//
+//  Created by Raymond Tellier on 2020-03-08.
+//  Copyright © 2020 Frap. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Selectors
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .twitterBlue
+    }
+}
